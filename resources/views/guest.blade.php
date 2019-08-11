4fc26b9bb58e8649e616b4e16868b2fe708a5c12
SelormBruce <developerin2020@gmail.com>
2019-08-11 05:58:25 -0700

    adding font awesome

--- resources/views/guest.blade.php
+++ resources/views/guest.blade.php
@@ -5,7 +5,9 @@
                     </ul>
 @guest
 <li class="nav-item">
-    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+    <a class="nav-link" href="{{ route('login') }}">
+ <i class="fa fa-user"></i>
+    </a>
 </li>
 {{-- @if (Route::has('register'))
     <li class="nav-item">
